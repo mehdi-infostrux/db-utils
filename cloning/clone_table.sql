efc038f8de819ee289d2efca4d2e53926e5906b2
mehdi-infostrux <112959205+mehdi-infostrux@users.noreply.github.com>
2023-03-15 11:08:02 +0100

    Create clone_table.sql
    Like the native snowflake CREATE CLONE this function clones a table from source to target. The difference is that it is reusable in a Snowflake SQL script, and it abstracts the EXECUTE IMMEDIATE statement.

---
+++ cloning/clone_table.sql
@@ -0,0 +1,11 @@
+CREATE OR REPLACE PROCEDURE MANAGEMENT.CLONING.CLONE_TEAM_TABLES(target_database_name VARCHAR(200), target_schema_name VARCHAR(200),  target_table_name VARCHAR(200),
+ source_database_name VARCHAR(200), source_schema_name VARCHAR(200),  source_table_name VARCHAR(200)  )
+RETURNS NUMBER(38,0)
+LANGUAGE SQL
+EXECUTE AS owner
+AS DECLARE
+  val VARCHAR(20);
+
+BEGIN
+    execute immediate 'CREATE OR REPLACE TABLE ' || target_database_name ||'.'||'"' || target_schema_name||'"'||'.'||target_table_name || ' CLONE  ' || source_database_name ||'.'|| '"'|| source_schema_name||'"'||'.'|| source_table_name ;
+END;
